68abbd16e8e7b33ae441489c265713038a4368b1
masahiro-endo <mendo@schit.co.jp>
2026-05-21 14:38:24 +0900

    test

---
+++ sample/01/main.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+os.chdir(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__))
+
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
